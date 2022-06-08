f8fe381b12cf0d46773091f548861a763f19fb44
Epiq122 <l2ob122@gmail.com>
2022-06-07 20:53:43 -0700

    finished info section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import profile from '../src/img/profile-removebg-preview.png';
 import Info from './components/info/Info';
 import About from './components/about/About';
 import Interests from './components/interests/Interests';
@@ -7,6 +8,7 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className='App'>
+      <img src={profile} alt='rob gleason' className='profile--img' />
       <Info />
       <About />
       <Interests />
--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,8 +1,18 @@
+import './Info.css';
+import mail from '../../img/Mail.png';
+import text from '../../img/Text.png';
+
 function Info() {
   return (
-    <div>
-      <h1>Info</h1>
-    </div>
+    <header>
+      <h1>Rob Gleason</h1>
+      <p className='job--p'>Full Stack Developer</p>
+      <p className='website--p'>https://robertgleason.netlify.app/</p>
+      <button className='email--btn'>
+        <img src={mail} alt='' className='mail--img' />
+        <img src={text} alt='' className='text-img' />
+      </button>
+    </header>
   );
 }
 
